2e8b3d725a2fd149064ac34d88168b85cd9d38e8
Danil Tsybulin <tsybulin2003@mail.ru>
2025-08-20 17:09:12 +0600

    Moved private routes to centralized load management:
    - components: [PrivateRoute] and [PublicRoute]

--- src/components/routes/PublicRoute.jsx
+++ src/components/routes/PublicRoute.jsx
@@ -2,30 +2,19 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { auth } from "../../firebase/firebase";
-import { RotateLoader } from "react-spinners";
 
 export default function PublicRoute({ children }) {
   const [user, setUser] = useState(undefined);
-  const [loader, setLoader] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       setUser(firebaseUser);
-      setTimeout(() => {
-        setLoader(false);
-      }, 600);
     });
 
     return () => unsubscribe();
   }, []);
 
-  if (user === undefined || loader) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <RotateLoader size={30} margin={20} color="#f59e0b" />
-      </div>
-    );
-  }
+  if (user === undefined) return null;
 
   return user ? <Navigate to="/" replace /> : children;
 }
