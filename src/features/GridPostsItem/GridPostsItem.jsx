cc71a539ac67ebb673a3b0a54c763248bf5ef9f3
Danil Tsybulin <tsybulin2003@mail.ru>
2025-09-04 11:23:04 +0600

    Improved grid post component:
    - added preview of the number of images (if there are more than 1 image)
    - added hover effect to improve UX so that users can see that they can navigate
    to the post via the images (this functionality is not currently available)

--- src/features/GridPostsItem/GridPostsItem.jsx
+++ src/features/GridPostsItem/GridPostsItem.jsx
@@ -1,10 +1,20 @@
-export default function Post({ src }) {
+export default function Post({ src, lengthImages }) {
   return (
-    <div className="rounded-2xl bg-zinc-900 border-1 border-zinc-400 aspect-square overflow-hidden">
-      <img
-        src={src}
-        className="w-full h-full max-w-full object-cover rounded-2xl"
-      />
+    <div
+      className="relative rounded-2xl overflow-hidden border border-zinc-600 aspect-square cursor-pointer 
+                    transition-transform transform  duration-500"
+    >
+      <img src={src} className="w-full h-full object-cover rounded-2xl" />
+
+      {lengthImages > 1 && (
+        <div className="absolute top-2 right-2 bg-black/60  text-white text-xs px-2 py-1 rounded">
+          1/{lengthImages}
+        </div>
+      )}
+
+      <div className="absolute inset-0 bg-black bg-opacity-20 duration-400 opacity-0 hover:opacity-70 transition-opacity flex items-center justify-center">
+        <span className="text-white text-lg font-semibold">Перейти</span>
+      </div>
     </div>
   );
 }
