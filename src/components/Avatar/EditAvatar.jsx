2cbcd8948c5503d2cdccdb287bb5a8a04ad6fee5
Danil Tsybulin <https://git.staffjet.ru/>
2025-09-24 12:11:39 +0600

    Create a UI for editing avatars:
    - When clicking on the [pencil icon] (change avatar), display a modal window with options to [add/delete] an avatar.
    - When clicking on [Add], the functionality to add a photo (previously added) is activated.

--- src/components/Avatar/EditAvatar.jsx
+++ src/components/Avatar/EditAvatar.jsx
@@ -6,6 +6,9 @@ import EditAvatarButton from "./EditAvatarButton";
 import DialogModal from "../DialogModal";
 import ImageCropperWrapper from "../Crop/ImageCropperWrapper";
 import { useSelector } from "react-redux";
+import ConfirmModal from "../ConfirmModal/ConfirmModal";
+import ConfirmButton from "../ConfirmModal/ConfirmButton";
+import CloseButton from "../CloseButton";
 
 export default function EditAvatar() {
   const fileInputRef = useRef(null);
@@ -13,15 +16,17 @@ export default function EditAvatar() {
   const { activeProfileUid, users, isCurrentUserProfile } = useSelector(
     (state) => state.users
   );
-
-  const [avatarPreview, setAvatarPreview] = useState(false);
-
   const avatarUrl = users[activeProfileUid]?.avatar_url || null;
 
-  const handleClick = () => {
-    fileInputRef.current?.click();
-  };
+  const [openConfirmModal, setOpenConfirmModal] = useState(false);
+  const [avatarPreview, setAvatarPreview] = useState(false);
 
+  //for add button
+  const handleAddAvatar = (closeConfirmModal) => {
+    fileInputRef.current?.click();
+
+    if (closeConfirmModal) setOpenConfirmModal(false);
+  };
   const handleFileChange = (e) => {
     const file = e.target.files[0];
 
@@ -39,25 +44,48 @@ export default function EditAvatar() {
     URL.revokeObjectURL(avatarPreview);
     setAvatarPreview(false);
   };
+
   return (
-    <div className="relative ">
+    <div className="relative">
       <AvatarIcon classNameSize="h-30 w-30" src={avatarUrl} />
 
-      {isCurrentUserProfile && !avatarUrl && (
+      {isCurrentUserProfile && (
         <EditAvatarButton
           ref={fileInputRef}
-          icon={<IoMdAdd className="w-5 h-5 text-black" />}
-          handleClick={handleClick}
+          icon={
+            avatarUrl ? (
+              <MdModeEdit className="w-5 h-5 text-black" />
+            ) : (
+              <IoMdAdd className="w-5 h-5 text-black" />
+            )
+          }
+          handleClick={
+            avatarUrl ? () => setOpenConfirmModal(true) : handleAddAvatar
+          }
           handleFileChange={handleFileChange}
         />
       )}
-      {isCurrentUserProfile && avatarUrl && (
-        <EditAvatarButton
-          ref={fileInputRef}
-          icon={<MdModeEdit className="w-5 h-5 text-black" />}
-          handleClick={handleClick}
-          handleFileChange={handleFileChange}
-        />
+
+      {openConfirmModal && (
+        <ConfirmModal
+          setOpenConfirmModal={setOpenConfirmModal}
+          confirmText="Изменить аватарку"
+        >
+          <div className="flex flex-col w-full gap-3">
+            <ConfirmButton
+              isGreen
+              text="Добавить новую"
+              onClick={() => handleAddAvatar(true)}
+            />
+            <ConfirmButton isRed text="Удалить аватарку" />
+          </div>
+
+          <CloseButton
+            right={10}
+            top={10}
+            onClick={() => setOpenConfirmModal(false)}
+          />
+        </ConfirmModal>
       )}
 
       <DialogModal isOpen={Boolean(avatarPreview)} widthCss="max-w-[50%]">
