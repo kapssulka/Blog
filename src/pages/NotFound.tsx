074b61ed25215549bf30f518bdbac229d959c61e
Danil Tsybulin <https://git.staffjet.ru/>
2025-10-24 11:16:36 +0600

    Type the NotFound components

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,17 +1,12 @@
-import ButtonOrange from "../components/ButtonOrange.js";
 import MainWrapper from "../components/layout/MainWrapper.js";
+import LinkOrange from "../components/orangeButton/LinkOrange.js";
 
 export default function NotFound() {
   return (
     <MainWrapper className="justify-center">
       <div className="flex flex-col items-center bg-zinc-900 h-full p-15 rounded-2xl mt-40">
         <h1 className="text-5xl">404 - Страница не найдена :(</h1>
-        <ButtonOrange
-          isLink
-          text="На главную"
-          className="max-w-[400px] w-full mt-10"
-          to="/"
-        />
+        <LinkOrange text="На главную" className="max-w-[400px] w-full mt-10" to="/" replace/>
       </div>
     </MainWrapper>
   );
