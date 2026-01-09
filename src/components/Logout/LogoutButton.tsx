f056e952b96e9a008ace063a8fdc9e7b119dee47
Danil Tsybulin <https://git.staffjet.ru/>
2026-01-09 10:55:23 +0600

    Add confirm modal for logout

--- src/components/Logout/LogoutButton.tsx
+++ src/components/Logout/LogoutButton.tsx
@@ -3,6 +3,9 @@ import { auth } from "../../firebase/firebase.js";
 import { useDispatch } from "react-redux";
 import { resetDataUser } from "../../redux/slices/currentUserSlice.js";
 import { resetAllBookmarks } from "../../redux/slices/postBookmarksSlice.js";
+import ConfirmModal from "../ConfirmModal/ConfirmModal.js";
+import ConfirmButton from "../ConfirmModal/ConfirmButton.js";
+import { useState } from "react";
 
 interface LogoutButtonProps {
   maxWidth?: string;
@@ -10,6 +13,9 @@ interface LogoutButtonProps {
 
 export default function LogoutButton({ maxWidth }: LogoutButtonProps) {
   const dispatch = useDispatch();
+
+  const [openConfirmModal, setOpenConfirmModal] = useState(false);
+
   const handleClick = () => {
     signOut(auth);
     dispatch(resetDataUser());
@@ -17,17 +23,31 @@ export default function LogoutButton({ maxWidth }: LogoutButtonProps) {
   };
 
   return (
-    <button
-      onClick={handleClick}
-      className={`w-full px-4 py-2 rounded-lg
+    <>
+      <button
+        onClick={() => setOpenConfirmModal(true)}
+        className={`w-full px-4 py-2 rounded-lg
                 cursor-pointer
                  bg-error
                  hover:bg-error-hover
                  text-white font-medium transition-colors duration-300 ${
                    maxWidth ? maxWidth : ""
                  }`}
-    >
-      Выйти
-    </button>
+      >
+        Выйти
+      </button>
+
+      <ConfirmModal
+        isOpen={openConfirmModal}
+        setOpenConfirmModal={setOpenConfirmModal}
+        confirmText="Вы действительно хотите выйти?"
+      >
+        <ConfirmButton
+          onClick={() => setOpenConfirmModal(false)}
+          text="Отмена"
+        />
+        <ConfirmButton onClick={handleClick} isRed text="Выйти" />
+      </ConfirmModal>
+    </>
   );
 }
