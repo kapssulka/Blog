0d8dd480985c21eff22720f801b56b1fdb904a68
Danil Tsybulin <https://git.staffjet.ru/>
2026-04-01 19:19:20 +0600

    Fixed the flickering of the header

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -7,16 +7,10 @@ import Logo from "../../components/Logo.js";
 
 export default function Header() {
   return (
-    <header
-      className="relative z-40 flex items-center 
-             bg-bg-secondary 
-             shadow-[0_4px_16px_rgba(0,0,0,0.3)]
-             h-[100px] rounded-b-2xl 
-             px-10 
-             backdrop-blur-sm"
-    >
+    <header className="relative z-40">
       {/* Для PC */}
-      <div className="flex gap-7 items-center w-full mx-auto max-w-[1020px] h-full justify-between py-3 bg-bg-secondary">
+
+      <div className="flex gap-7 px-10 rounded-b-2xl items-center w-full mx-auto max-w-[1020px] h-[100px] justify-between py-3 bg-bg-secondary">
         {/* Лого */}
         <Logo />
         <div className="hidden md:flex items-center gap-8">
