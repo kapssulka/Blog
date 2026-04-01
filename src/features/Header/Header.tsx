1313e4ac1889a748565b1f3a98c6d6ac2b9caae5
Danil Tsybulin <https://git.staffjet.ru/>
2026-04-01 19:19:20 +0600

    Fixed the flickering of the header

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -7,32 +7,27 @@ import Logo from "../../components/Logo.js";
 
 export default function Header() {
   return (
-    <header
-      className="relative z-40 flex items-center 
-             bg-bg-secondary 
-             shadow-[0_4px_16px_rgba(0,0,0,0.3)]
-             h-[100px] rounded-b-2xl 
-             px-10 
-             backdrop-blur-sm"
-    >
+    <header className="relative z-40 h-[100px]">
       {/* Для PC */}
-      <div className="flex gap-7 items-center w-full mx-auto max-w-[1020px] h-full justify-between py-3 bg-bg-secondary">
-        {/* Лого */}
-        <Logo />
-        <div className="hidden md:flex items-center gap-8">
-          {/* Навигация */}
-          <nav className="hidden items-center gap-6 md:flex">
-            <NavItem to={ROUTES.ABOUT_PROJECT}>О проекте</NavItem>
-          </nav>
+      <div className="bg-bg-secondary px-10 py-3 h-full">
+        <div className="flex gap-7 rounded-b-2xl items-center w-full mx-auto h-full max-w-[1020px] justify-between">
+          {/* Лого */}
+          <Logo />
+          <div className="hidden md:flex items-center gap-8">
+            {/* Навигация */}
+            <nav className="hidden items-center gap-6 md:flex">
+              <NavItem to={ROUTES.ABOUT_PROJECT}>О проекте</NavItem>
+            </nav>
 
-          {/* Правая часть */}
-          <div className="flex items-center gap-4">
-            <LogoutButton />
+            {/* Правая часть */}
+            <div className="flex items-center gap-4">
+              <LogoutButton />
+            </div>
           </div>
-        </div>
 
-        {/* Для Мобилок */}
-        <BurgerMenu />
+          {/* Для Мобилок */}
+          <BurgerMenu />
+        </div>
       </div>
     </header>
   );
